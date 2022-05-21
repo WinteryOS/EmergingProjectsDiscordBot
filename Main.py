091606223ea77339a9d8181d2057edc4a15b169c
JustinW1233 <79864211+JustinW1233@users.noreply.github.com>
2022-05-20 18:06:03 -0600

    Completed the image search function

--- Main.py
+++ Main.py
@@ -1,18 +1,65 @@
-# bot.py
+from http import client
 import os
-import discord
+from urllib import response
+from urllib.request import urlopen
+from discord.ext import commands
+import re
 
 from dotenv import load_dotenv
 load_dotenv('.env')
 
-client = discord.Client()
+client = commands.Bot(command_prefix='$')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+global item_num
+item_num = 0
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/1200px-A_small_cup_of_coffee.JPG')
+
+@client.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+
+@client.command()
+async def img(ctx, arg):
+    url = "https://serpapi.com/search.json?engine=google&q="+arg+"&google_domain=google.com&gl=us&hl=en&tbm=isch&api_key=e943bb910496b0c2f927da2a95bc84819d19c75b8811a84d6375792693177796"
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    response = urlopen(url)
+    #data_json = json.loads(response.read())
+    data_json = response.read()
+    #print(data_json)
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+
+    #IMG_REG = '(images_results\": )(\[((.*)([\n\t]))*\])'
+    IMG_REG = r"thumbnail\": \"(https:.+?)\""
+    matches = re.findall(IMG_REG, str(data_json))
+    for match in matches:
+        print(match)
     
-    if message.content.startswith('$Are you a robot Goigle'):
-        await message.channel.send('Heavens no, I\'m the real deal')
+    for i in range(item_num, item_num+5):
+        await ctx.send(matches[i])
+
+@client.command()
+async def group_img(ctx, arg):
+    url = "https://serpapi.com/search.json?engine=google&q="+arg+"&google_domain=google.com&gl=us&hl=en&tbm=isch&api_key=e943bb910496b0c2f927da2a95bc84819d19c75b8811a84d6375792693177796"
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    response = urlopen(url)
+    #data_json = json.loads(response.read())
+    data_json = response.read()
+    #print(data_json)
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+
+    #IMG_REG = '(images_results\": )(\[((.*)([\n\t]))*\])'
+    IMG_REG = r"thumbnail\": \"(https:.+?)\""
+    matches = re.findall(IMG_REG, str(data_json))
+    for match in matches:
+        print(match)
+    
+    for i in range(item_num, item_num+5):
+        await ctx.send(matches[i])
+    
 
 client.run(os.getenv('TOKEN'))
