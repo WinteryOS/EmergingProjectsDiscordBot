775da8c8ea526be645c224af24d32735c87338b6
ArturoDuran90 <78046770+ArturoDuran90@users.noreply.github.com>
2022-05-23 16:29:37 -0600

    working but no user input

--- serpApiUrl.py
+++ serpApiUrl.py
@@ -1,22 +1,36 @@
+from ast import keyword
 import os
 import requests
 import discord
 from serpapi import GoogleSearch
 import json
 
-api_Key = "9a44608178a3b7ad9888bb12ed05a1992916835b8af2d5bc1fc164a5f8b1201d";
-googleSearch = "Coffee";
+#SERPAPI_KEY = "9a44608178a3b7ad9888bb12ed05a1992916835b8af2d5bc1fc164a5f8b1201d";
+keywords = [
+  "Coffee",
+  "Twitter"
+]
 
-response_API = requests.get('https://serpapi.com/search.json?engine=google&q=' + googleSearch + '&location=Salt+Lake+City%2C+Utah%2C+United+States&google_domain=google.com&gl=us&hl=en&api_key=' + api_Key)
+#search = requests.get('https://serpapi.com/search.json?engine=google&q=' + keywords + '&location=Salt+Lake+City%2C+Utah%2C+United+States&google_domain=google.com&gl=us&hl=en&api_key=' + api_Key)
 
-data = response_API.text
-parse_json = json.loads(data)
-link = parse_json['organic_results']['']['link']
+search = GoogleSearch({"api_key": os.getenv("SERPAPI_KEY")})
+# data = response_API.text
+# parse_json = json.loads(data)
+# link = parse_json['organic_results']['']['link']
 
-for dict in parse_json:
-  manipulate dict
+for keyword in keywords:
+  search.params_dict['q'] = keyword
+  result = search.get_dict()
 
-print("Link", link)
+  print(f"Top 5 results for '{keyword}':")
+
+  for organic_results in result.get("organic_results", [])[:5]:
+    position = organic_results.get("position", None)
+    title = organic_results.get("title", "")
+    link = organic_results.get("link", "")
+
+    print(f"Title: {title}\nPosition: {position}\nLink: {link}\n\n")
+
 
 # params = {
 #     "q": "Coffee",
